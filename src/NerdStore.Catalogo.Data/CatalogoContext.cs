83267d27e51ef12d5172ff885ed21df5238ddec3
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-22 18:07:04 -0300

    Catalogo.Data - Implementação do Pacote

---
+++ src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using NerdStore.Core.Messages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data
+{
+    public class CatalogoContext : DbContext, IUnitOfWork
+    {
+        public CatalogoContext(DbContextOptions<CatalogoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                x => x.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                // Update (EF Core 3.x) or higher
+                property.SetColumnType("varchar(100)");
+            }
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/NerdStore.Catalogo.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Domain;
+using NerdStore.Core.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Data.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public ProdutoRepository(CatalogoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _context.Produtos
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Produto> ObterPorId(Guid id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterPorCategoria(int codigo)
+        {
+            return await _context.Produtos
+                .AsNoTracking()
+                .Include(i => i.Categoria)
+                .Where(p => p.Categoria.Codigo == codigo)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Categoria>> ObterCategorias()
+        {
+            return await _context.Categorias
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+        }
+
+        public void Adicionar(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+        }
+
+        public void Atualizar(Categoria categoria)
+        {
+            _context.Categorias.Update(categoria);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
