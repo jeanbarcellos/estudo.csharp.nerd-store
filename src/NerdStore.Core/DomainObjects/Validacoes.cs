1e454e51fc364a15370431da54980144d38117f9
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-22 14:23:31 -0300

    Core - Criação de DomainObjects

---
+++ src/NerdStore.Core/DomainObjects/Validacoes.cs
@@ -0,0 +1,156 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NerdStore.Core.DomainObjects
+{
+    // public class AssertionConcern
+    public class Validacoes
+    {
+        public static void ValidarSeIgual(object objeto1, object objeto2, string mensagem)
+        {
+            if (objeto1.Equals(objeto2))
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeDiferente(object objeto1, object objeto2, string mensagem)
+        {
+            if (!objeto1.Equals(objeto2))
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeDiferente(string expressao, string valor, string mensagem)
+        {
+            var regex = new Regex(expressao);
+
+            if (!regex.IsMatch(valor))
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarTamanho(string valor, int maximo, string mensagem)
+        {
+            var length = valor.Trim().Length;
+            if (length > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarTamanho(string valor, int minimo, int maximo, string mensagem)
+        {
+            var length = valor.Trim().Length;
+            if (length < minimo || length > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeVazio(string valor, string mensagem)
+        {
+            if (valor == null || valor.Trim().Length == 0)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeNulo(object objeto1, string mensagem)
+        {
+            if (objeto1 == null)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarMinimoMaximo(double valor, double minimo, double maximo, string mensagem)
+        {
+            if (valor < minimo || valor > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarMinimoMaximo(float valor, float minimo, float maximo, string mensagem)
+        {
+            if (valor < minimo || valor > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarMinimoMaximo(int valor, int minimo, int maximo, string mensagem)
+        {
+            if (valor < minimo || valor > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarMinimoMaximo(long valor, long minimo, long maximo, string mensagem)
+        {
+            if (valor < minimo || valor > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarMinimoMaximo(decimal valor, decimal minimo, decimal maximo, string mensagem)
+        {
+            if (valor < minimo || valor > maximo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeMenorQue(long valor, long minimo, string mensagem)
+        {
+            if (valor < minimo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeMenorQue(double valor, double minimo, string mensagem)
+        {
+            if (valor < minimo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeMenorQue(decimal valor, decimal minimo, string mensagem)
+        {
+            if (valor < minimo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeMenorQue(int valor, int minimo, string mensagem)
+        {
+            if (valor < minimo)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeFalso(bool boolvalor, string mensagem)
+        {
+            if (!boolvalor)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+
+        public static void ValidarSeVerdadeiro(bool boolvalor, string mensagem)
+        {
+            if (boolvalor)
+            {
+                throw new DomainException(mensagem);
+            }
+        }
+    }
+}
