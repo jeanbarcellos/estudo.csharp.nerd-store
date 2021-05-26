f28d19dfd9d2453a8df3db451d899ac79e3907be
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-25 22:59:29 -0300

    database - Adição dos dados do contexto Catalogo

---
+++ database/CatalogContext.sql
@@ -0,0 +1,19 @@
+-- ----------------------------
+-- Records of Categorias
+-- ----------------------------
+INSERT INTO "public"."Categorias" VALUES ('06867776-b52c-4a85-8815-14ee1552a559', 'Camisetas', 100);
+INSERT INTO "public"."Categorias" VALUES ('18e5b82c-6c4c-42ef-ab9f-5f142ecd663e', 'Canecas', 101);
+INSERT INTO "public"."Categorias" VALUES ('e55d2134-b9ae-4921-bf05-b084e29248e1', 'Adeviso', 102);
+
+
+-- ----------------------------
+-- Records of Produtos
+-- ----------------------------
+INSERT INTO "public"."Produtos" VALUES ('06867776-b52c-4a85-8815-14ee1552a559', '06867776-b52c-4a85-8815-14ee1552a559', 'Camiseta Sofware Developer', 'Descrição', 't', 100, '2021-02-27 17:10:21.116423', 'camiseta1.jpg', 8, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('c498acc1-b622-4507-b3ff-64084325635e', '06867776-b52c-4a85-8815-14ee1552a559', 'Camiseta Code Life Preta', 'Descrição', 't', 90, '2021-02-27 00:00:00', 'camiseta2.jpg', 3, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('09c0e6b1-4763-4c77-bb92-ee3f68f8995e', '06867776-b52c-4a85-8815-14ee1552a559', 'Camiseta Code Life Cinza', 'Descrição', 't', 80, '2021-02-27 17:08:52.913199', 'camiseta3.jpg', 5, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('82e966fe-806e-4395-97af-20bcf4f31733', '06867776-b52c-4a85-8815-14ee1552a559', 'Camiseta Debugar Preta', 'Descrição', 't', 110, '2021-02-27 00:00:00', 'camiseta4.jpg', 5, 11, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('55c0eb46-8eee-441d-afc6-a8ba3d2881df', '18e5b82c-6c4c-42ef-ab9f-5f142ecd663e', 'Caneca Start Bugs', 'Descrição', 't', 25, '2021-02-27 17:10:21.116423', 'caneca1.jpg', 8, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('ac6deb27-e7c9-4169-99f9-787b28283573', '18e5b82c-6c4c-42ef-ab9f-5f142ecd663e', 'Caneca Programmer COde', 'Descrição', 't', 15, '2021-02-27 17:14:35.909087', 'caneca2.jpg', 5, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('0a988a84-3a2e-41bb-b8d7-7fb16a48cad0', '18e5b82c-6c4c-42ef-ab9f-5f142ecd663e', 'Caneca Turn Coffe In Code', 'Descrição', 't', 15, '2021-02-27 17:10:21.116423', 'caneca3.jpg', 8, 5, 5, 5);
+INSERT INTO "public"."Produtos" VALUES ('399a4305-1172-433a-891b-e0ac3857cde0', '18e5b82c-6c4c-42ef-ab9f-5f142ecd663e', 'Caneca No Coffe No Code', 'Descrição', 't', 30, '2021-02-27 17:10:21.116423', 'caneca4.jpg', 8, 5, 5, 5);
