70bfc0886ca4e32e8242b501fba69893e61dc452
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-29 15:42:22 -0300

    Core - Adição de Extensions e Messages

---
+++ src/NerdStore.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NerdStore.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+                action(item);
+        }
+    }
+}
---
+++ src/NerdStore.Core/Messages/CommonMessages/IntegrationEvents/IntegrationEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Core.Messages.CommonMessages.IntegrationEvents
+{
+    public abstract class IntegrationEvent : Event
+    {
+
+    }
+}
