4a0011c5660da92b6dc15e985e9e58ca6b1c42a0
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-05-29 15:43:24 -0300

    Core - Adição de Mensagens/Eventos de Integração

---
+++ src/NerdStore.Core/Messages/CommonMessages/IntegrationEvents/PedidoProcessamentoCanceladoEvent.cs
@@ -0,0 +1,20 @@
+﻿using NerdStore.Core.DomainObjects.DTO;
+using System;
+
+namespace NerdStore.Core.Messages.CommonMessages.IntegrationEvents
+{
+    public class PedidoProcessamentoCanceladoEvent : IntegrationEvent
+    {
+        public Guid PedidoId { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public ListaProdutosPedido ProdutosPedido { get; private set; }
+
+        public PedidoProcessamentoCanceladoEvent(Guid pedidoId, Guid clienteId, ListaProdutosPedido produtosPedido)
+        {
+            AggregateId = pedidoId;
+            PedidoId = pedidoId;
+            ClienteId = clienteId;
+            ProdutosPedido = produtosPedido;
+        }
+    }
+}
