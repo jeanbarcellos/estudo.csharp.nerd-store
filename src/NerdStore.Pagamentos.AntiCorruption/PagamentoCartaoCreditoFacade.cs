a1e45a35d27829ebb9e4917122516b9b0b4159b1
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 23:07:38 -0300

    Pagamentos.AntiCurruption - Implementação da Interface Facade de BUssines

---
+++ src/NerdStore.Pagamentos.AntiCorruption/PagamentoCartaoCreditoFacade.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using NerdStore.Pagamentos.Business;
+
+namespace NerdStore.Pagamentos.AntiCorruption
+{
+    public class PagamentoCartaoCreditoFacade : IPagamentoCartaoCreditoFacade
+    {
+        private readonly IPayPalGateway _payPalGateway;
+        private readonly IConfigurationManager _configurationManager;
+
+        public PagamentoCartaoCreditoFacade(
+            IPayPalGateway payPalGateway,
+            IConfigurationManager configurationManager
+        )
+        {
+            _payPalGateway = payPalGateway;
+            _configurationManager = configurationManager;
+        }
+
+        public Transacao RealizarPagamento(Pedido pedido, Pagamento pagamento)
+        {
+            var apiKey = _configurationManager.GetValue("apiKey");
+            var encKey = _configurationManager.GetValue("encKey");
+
+            var serviceKey = _payPalGateway.GetPayPalServiceKey(apiKey, encKey);
+            var cardHashKey = _payPalGateway.GetCardHashKey(serviceKey, pagamento.NumeroCartao);
+
+            var pagamentoResultado = _payPalGateway.CommitTransaction(cardHashKey, pedido.Id.ToString(), pagamento.Valor);
+
+            // TODO: informacao retornada pelo gateway
+            var transacao = new Transacao
+            {
+                PedidoId = pedido.Id,
+                PagamentoId = pagamento.Id,
+                Total = pedido.Valor
+            };
+
+            if (pagamentoResultado)
+            {
+                transacao.StatusTransacao = StatusTransacao.Pago;
+                return transacao;
+            }
+
+            transacao.StatusTransacao = StatusTransacao.Recusado;
+            return transacao;
+        }
+
+    }
+}
