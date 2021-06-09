a571053344f138a4d22e0ad5d52333331daf8aea
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 22:25:02 -0300

    database - adição de VendasContext.sql

---
+++ database/VendasContext.sql
@@ -0,0 +1,5 @@
+-- ----------------------------
+-- Records of Categorias
+-- ----------------------------
+INSERT INTO "public"."Vouchers"("Id", "Codigo", "Percentual", "ValorDesconto", "Quantidade", "TipoDescontoVoucher", "DataCriacao", "DataUtilizacao", "DataValidade", "Ativo", "Utilizado") VALUES ('20bc1b93-5111-4d32-ab3e-779222bbdff9', 'PROMO-10-OFF', '10.00', NULL, 50, 0, '2021-02-01 00:00:00', NULL, '2021-12-31 00:00:00', 't', 'f');
+INSERT INTO "public"."Vouchers"("Id", "Codigo", "Percentual", "ValorDesconto", "Quantidade", "TipoDescontoVoucher", "DataCriacao", "DataUtilizacao", "DataValidade", "Ativo", "Utilizado") VALUES ('3ccbaf0b-9793-42cf-a27c-6f51c93e95d6', 'PROMO-15-REAIS', NULL, '15.00', 50, 1, '2021-02-01 00:00:00', NULL, '2021-12-31 00:00:00', 't', 'f');
