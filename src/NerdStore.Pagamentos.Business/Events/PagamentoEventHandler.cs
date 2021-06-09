0c499f49dd7d22f7dd292db8a2d9847cfac17727
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 23:01:57 -0300

    Pagamentos.Bussiness - Implementação dos Eventos e Handlers

---
+++ src/NerdStore.Pagamentos.Business/Events/PagamentoEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using NerdStore.Core.Messages.CommonMessages.IntegrationEvents;
+using NerdStore.Core.DomainObjects.DTO;
+
+namespace NerdStore.Pagamentos.Business.Events
+{
+    public class PagamentoEventHandler : INotificationHandler<PedidoEstoqueConfirmadoEvent>
+    {
+        private readonly IPagamentoService _pagamentoService;
+
+        public PagamentoEventHandler(IPagamentoService pagamentoService)
+        {
+            _pagamentoService = pagamentoService;
+        }
+
+        public async Task Handle(PedidoEstoqueConfirmadoEvent message, CancellationToken cancellationToken)
+        {
+            var pagamentoPedido = new PagamentoPedido {
+                PedidoId = message.PedidoId,
+                ClienteId = message.ClienteId,
+                Total = message.Total,
+                NomeCartao = message.NomeCartao,
+                NumeroCartao = message.NumeroCartao,
+                ExpiracaoCartao = message.ExpiracaoCartao,
+                CVVCartao = message.CVVCartao
+            };
+
+            await _pagamentoService.RealizarPagamentoPedido(pagamentoPedido);
+        }
+    }
+}
