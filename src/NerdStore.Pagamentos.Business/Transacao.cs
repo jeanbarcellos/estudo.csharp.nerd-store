f735043b4152f439fb98b6bc0b9fb97a927542a4
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 22:42:49 -0300

    Pagamentos.Bussines - Criação das Entidades, VOs e ENUMs

---
+++ src/NerdStore.Pagamentos.Business/Transacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Pagamentos.Business
+{
+    public class Transacao : Entity
+    {
+        public Guid PedidoId { get; set; }
+        public Guid PagamentoId { get; set; }
+        public decimal Total { get; set; }
+        public StatusTransacao StatusTransacao { get; set; }
+
+        public virtual Pagamento Pagamento { get; set; }
+
+    }
+}
