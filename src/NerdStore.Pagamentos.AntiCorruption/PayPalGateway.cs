6b312c83023629b2b490d5c0f0589fc4e0f7bdf6
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 23:06:46 -0300

    Pagamentos.AntiCorruption - Implementação do Getway para o PayPal

---
+++ src/NerdStore.Pagamentos.AntiCorruption/PayPalGateway.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace NerdStore.Pagamentos.AntiCorruption
+{
+    public class PayPalGateway : IPayPalGateway
+    {
+        public bool CommitTransaction(string cardHashKey, string orderId, decimal amount)
+        {
+            return new Random().Next(2) == 0;
+        }
+
+        public string GetCardHashKey(string serviceKey, string cartaoCredito)
+        {
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+                .Select(x => x[new Random().Next(x.Length)]).ToArray());
+        }
+
+        public string GetPayPalServiceKey(string apiKey, string encriptionKey)
+        {
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+                .Select(x => x[new Random().Next(x.Length)]).ToArray());
+        }
+    }
+}
