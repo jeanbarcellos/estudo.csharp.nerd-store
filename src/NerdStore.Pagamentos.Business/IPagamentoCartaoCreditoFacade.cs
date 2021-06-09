c1c90bca0914a312498da2c40ee66c03cc63315e
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-08 22:53:05 -0300

    Pagamentos.Bussines - Criação das interfaces Service e Facade

---
+++ src/NerdStore.Pagamentos.Business/IPagamentoCartaoCreditoFacade.cs
@@ -0,0 +1,9 @@
+﻿using NerdStore.Pagamentos.Business;
+
+namespace NerdStore.Pagamentos.Business
+{
+    public interface IPagamentoCartaoCreditoFacade
+    {
+        Transacao RealizarPagamento(Pedido pedido, Pagamento pagamento);
+    }
+}
