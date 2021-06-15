38bd3298cd392431bdfba7a4d525c1c696bee039
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-14 21:53:23 -0300

    WebApp.MVC - Exibição dos pedidos

---
+++ src/NerdStore.WebApp.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MediatR;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using NerdStore.Vendas.Application.Queries;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        public PedidoController(
+            IPedidoQueries pedidoQueries,
+            IMediatorHandler mediatorHandler,
+            INotificationHandler<DomainNotification> notifications
+        )
+            : base(notifications, mediatorHandler)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        [Route("meus-pedidos")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _pedidoQueries.ObterPedidosCliente(ClienteId));
+        }
+    }
+}
