16197a239e63a048c39a283dda7a0abea4387fac
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-21 21:03:53 -0300

    WebApp.API - Ajustes

---
+++ src/NerdStore.WebApp.API/Controllers/ControllerBase.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public abstract class ControllerBase : Controller
+    {
+        public Guid ClienteId => Guid.Parse("7030F1D4-A952-4B27-B323-7277615621FB");
+
+        private readonly DomainNotificationHandler _notifications;
+        private readonly IMediatorHandler _mediatorHandler;
+
+        protected ControllerBase(
+            INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediatorHandler
+        )
+        {
+            _notifications = (DomainNotificationHandler)notifications;
+            _mediatorHandler = mediatorHandler;
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !_notifications.TemNotificacao();
+        }
+
+        protected IEnumerable<string> ObterMensagensErro()
+        {
+            return _notifications.ObterNotificacoes().Select(x => x.Value).ToList();
+        }
+
+        protected void NotificarErro(string codigo, string mensagem)
+        {
+            _mediatorHandler.PublicarNotificacao(new DomainNotification(codigo, mensagem));
+        }
+
+        protected new IActionResult Response(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                success = false,
+                errors = _notifications.ObterNotificacoes().Select(n => n.Value)
+            });
+        }
+    }
+}
--- src/NerdStore.WebApp.API/Startup.cs
+++ src/NerdStore.WebApp.API/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NerdStore.WebApp.API
 {
@@ -23,7 +16,6 @@ namespace NerdStore.WebApp.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -34,7 +26,6 @@ namespace NerdStore.WebApp.API
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
