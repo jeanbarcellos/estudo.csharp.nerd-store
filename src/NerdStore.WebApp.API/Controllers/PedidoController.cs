97fbf3c020534c58ae92f45bd72536a4c07b6773
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-27 17:35:21 -0300

    WebApp.API - Crição do Controller PedidoController

---
+++ src/NerdStore.WebApp.API/Controllers/PedidoController.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NerdStore.WebApp.API.Controllers
+{
+    [Authorize]
+    public class PedidoController : ControllerBase
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        public PedidoController(
+            IPedidoQueries pedidoQueries,
+            IMediatorHandler mediatorHandler,
+            INotificationHandler<DomainNotification> notifications,
+            IHttpContextAccessor httpContextAccessor
+        )
+            : base(notifications, mediatorHandler, httpContextAccessor)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        [HttpGet]
+        [Route("api/pedidos")]
+        public async Task<IActionResult> Index()
+        {
+            return Response(await _pedidoQueries.ObterPedidosCliente(ClienteId));
+        }
+    }
+}
