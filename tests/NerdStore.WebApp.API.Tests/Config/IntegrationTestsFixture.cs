b5bfc80ab834b2264f33444222569d0cedf56886
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-28 21:05:21 -0300

    WebApp.Tests - Cração da configuração básica para o teste de integração

---
+++ tests/NerdStore.WebApp.API.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http;
+using Xunit;
+
+namespace NerdStore.WebApp.API.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationTestsFictureCollection))]
+    public class IntegrationTestsFictureCollection : ICollectionFixture<IntegrationTestsFixture<StartupTests>> { }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            var clientOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient(clientOptions);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ tests/NerdStore.WebApp.API.Tests/UsuarioTests.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.WebApp.API.Tests.Config;
+
+namespace NerdStore.WebApp.API.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupTests> _textsFixture;
+
+        public UsuarioTests(IntegrationTestsFixture<StartupTests> textsFixture)
+        {
+            _textsFixture = textsFixture;
+        }
+    }
+}
