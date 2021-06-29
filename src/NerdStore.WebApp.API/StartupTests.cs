7d4ebb202218dae2592a2c597a5f03ba7907d7a6
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-28 21:04:56 -0300

    WebApp.API - Criação de Startup para testes

---
+++ src/NerdStore.WebApp.API/StartupTests.cs
@@ -0,0 +1,72 @@
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NerdStore.WebApp.API.Configurations;
+
+namespace NerdStore.WebApp.API
+{
+    public class StartupTests
+    {
+        public IConfiguration Configuration { get; }
+
+        public StartupTests(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // WebAPI Settings
+            services.AddControllers();
+
+            // DBContexts Settings
+            services.AddDatabaseConfiguration(Configuration);
+
+            // ASP.NET Identity Settings & JWT
+            services.AddIdentityConfiguration(Configuration);
+
+            // AutoMapper Settings
+            services.AddAutoMapperConfiguration();
+
+            // Adding MediatR for Domain Events and Notifications
+            services.AddMediatR(typeof(Startup));
+
+            // DI Abstraction
+            services.AddDependencyInjectionConfiguration();
+
+            // Swagger Settings
+            services.AddSwaggerConfiguration();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(c =>
+            {
+                c.AllowAnyHeader();
+                c.AllowAnyMethod();
+                c.AllowAnyOrigin();
+            });
+
+            app.UseAuthConfiguration();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwaggerSetup();
+        }
+    }
+}
