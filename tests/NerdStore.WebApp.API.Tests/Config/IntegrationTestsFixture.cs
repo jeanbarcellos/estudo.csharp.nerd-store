72c2315721c837973e282902d9b5e43968b33de8
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-06-28 22:07:16 -0300

    Web.API - Ajustes nas Configurações

--- tests/NerdStore.WebApp.API.Tests/Config/IntegrationTestsFixture.cs
+++ tests/NerdStore.WebApp.API.Tests/Config/IntegrationTestsFixture.cs
@@ -1,27 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.API.Models;
 using System;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace NerdStore.WebApp.API.Tests.Config
 {
-    [CollectionDefinition(nameof(IntegrationTestsFictureCollection))]
-    public class IntegrationTestsFictureCollection : ICollectionFixture<IntegrationTestsFixture<StartupTests>> { }
+    [CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
+    public class IntegrationTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupTests>> { }
 
     public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
     {
         public readonly LojaAppFactory<TStartup> Factory;
         public HttpClient Client;
 
+        public string UsuarioToken;
+
         public IntegrationTestsFixture()
         {
             var clientOptions = new WebApplicationFactoryClientOptions
             {
-
+                AllowAutoRedirect = true,
+                BaseAddress = new Uri("http://localhost"),
+                HandleCookies = true,
+                MaxAutomaticRedirections = 7
             };
 
             Factory = new LojaAppFactory<TStartup>();
             Client = Factory.CreateClient(clientOptions);
+        }
+
+        public async Task RealizarLogin()
+        {
+            var userData = new LoginViewModel
+            {
+                Email = "teste@teste.com",
+                Senha = "Teste@123"
+            };
+
+            // Recriando o client para evitar configurações de Web
+            Client = Factory.CreateClient();
+
+            var response = await Client.PostAsJsonAsync("api/login", userData);
+            response.EnsureSuccessStatusCode();
+
+            UsuarioToken = await response.Content.ReadAsStringAsync();
         }
 
         public void Dispose()
---
+++ tests/NerdStore.WebApp.API.Tests/Config/TestsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace NerdStore.WebApp.API.Tests.Config
+{
+    public static class TestsExtensions
+    {
+        public static void AtribuirToken(this HttpClient client, string token)
+        {
+            client.AtribuirJsonMediaType();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public static void AtribuirJsonMediaType(this HttpClient client)
+        {
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
