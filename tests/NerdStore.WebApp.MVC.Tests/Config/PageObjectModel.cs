027f509feb94a5584585e72e8bb32ab7af3b4d4d
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-05 20:32:44 -0300

    Criação de PageObjectModel

---
+++ tests/NerdStore.WebApp.MVC.Tests/Config/PageObjectModel.cs
@@ -0,0 +1,22 @@
+﻿namespace NerdStore.WebApp.MVC.Tests.Config
+{
+    public class PageObjectModel
+    {
+        protected readonly SeleniumHelper Helper;
+
+        protected PageObjectModel(SeleniumHelper helper)
+        {
+            Helper = helper;
+        }
+
+        public string ObterUrl()
+        {
+            return Helper.ObterUrl();
+        }
+
+        public void NavegarParaUrl(string url)
+        {
+            Helper.IrParaUrl(url);
+        }
+    }
+}
