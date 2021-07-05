5f8f2bda2bf9b9263c97526701c4acf3a5caa812
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-07-05 20:31:23 -0300

    Adição de extras

---
+++ tests/NerdStore.WebApp.MVC.Tests/Config/AutomacaoWebTestsFixture.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace NerdStore.WebApp.MVC.Tests.Config
+{
+    [CollectionDefinition(nameof(AutomacaoWebFixtureCollection))]
+    public class AutomacaoWebFixtureCollection : ICollectionFixture<AutomacaoWebTestsFixture> { }
+
+    public class AutomacaoWebTestsFixture
+    {
+        public SeleniumHelper BrowserHelper;
+        public readonly ConfigurationHelper Configuration;
+
+        public AutomacaoWebTestsFixture()
+        {
+            Configuration = new ConfigurationHelper();
+            BrowserHelper = new SeleniumHelper(Browser.Chrome, Configuration);
+        }
+    }
+}
---
+++ tests/NerdStore.WebApp.MVC.Tests/Config/TestsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace NerdStore.WebApp.MVC.Tests.Config
+{
+    public static class TestsExtensions
+    {
+        public static int ApenasNumeros(this string value)
+        {
+            return Convert.ToInt16(new string(value.Where(char.IsDigit).ToArray()));
+        }
+    }
+}
